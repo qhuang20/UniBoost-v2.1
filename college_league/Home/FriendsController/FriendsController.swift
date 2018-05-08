a82793053a6d49810b785a998557ce938c4185f2
Jeffery Huang <jeffery@mymac.local>
2018-05-08 11:33:05 -0700

    //

---
+++ college_league/Home/FriendsController/FriendsController.swift
@@ -0,0 +1,18 @@
+//
+//  FriendsController.swift
+//  college_league
+//
+//  Created by Qichen Huang on 2018-05-08.
+//  Copyright © 2018 Qichen Huang. All rights reserved.
+//
+
+import UIKit
+
+class FriendsController: UICollectionViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        collectionView?.backgroundColor = UIColor.blue
+    }
+    
+}
